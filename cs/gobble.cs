5e734c2fa6afaad36fa196c871047244a49a8b96
Nekoboi <dev@nekoboi.com>
2021-01-16 13:00:45 -0500

    Create gobble.cs

---
+++ cs/gobble.cs
@@ -0,0 +1,10 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            System.Console.WriteLine("Microsoft Corporation is coming to gobble your toes!!!");
+        }
+    }
+}
