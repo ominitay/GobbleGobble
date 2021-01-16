508681babe50adb8c6f2d8b9f4e0c3319bbc235c
Schyrsivochter <andreas.kuebrich@kuebrich.de>
2021-01-16 22:15:02 +0100

    Add Java and Kotlin

---
+++ java/Gobble.java
@@ -0,0 +1,7 @@
+public class Gobble {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("James Gosling is coming to gobble your toes!!!");
+        }
+    }
+}
---
+++ kt/gobble.kt
@@ -0,0 +1,8 @@
+tailrec fun gobble() {
+    println("Czech coders are coming to gobble your toes!")
+    gobble()
+}
+
+fun main() {
+    gobble()
+}
