e97714c639422792c41b5b497062e6bf22fd7368
ominitay <37453713+ominitay@users.noreply.github.com>
2022-01-04 00:38:19 +0000

    fix typo

--- zig/gobble.zig
+++ zig/gobble.zig
@@ -4,6 +4,6 @@ const stdout = std.io.getStdOut().writer();
 
 pub fn main() !void {
     while (true) {
-        try stdout.print("{s} is coming to gobble your toes!!!\n", .{"Andrew Kelly"});
+        try stdout.print("{s} is coming to gobble your toes!!!\n", .{"Andrew Kelley"});
     }
 }
