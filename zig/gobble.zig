fe7e7f334489e1ce969091e248fb614b6dbdeb78
ominitay <37453713+ominitay@users.noreply.github.com>
2022-01-04 00:42:16 +0000

    fix windows

--- zig/gobble.zig
+++ zig/gobble.zig
@@ -1,8 +1,7 @@
 const std = @import("std");
 
-const stdout = std.io.getStdOut().writer();
-
 pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
     while (true) {
         try stdout.print("{s} is coming to gobble your toes!!!\n", .{"Andrew Kelley"});
     }
